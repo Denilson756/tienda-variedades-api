12920fc6227a78b4cd2a3d448f4f28c18b852d6f
Denilson756 <dancuentapersonal@gmail.com>
2026-08-14 02:18:48 -0500

    Login de usuario con JWT funcionando

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -13,4 +13,32 @@ async function registrar(req, res) {
     }
 }
 
-module.exports = {registrar};
+const jwt = require("jsonwebtoken"); // Importa el paquete instalado
+
+async function login(req, res) {
+    try {
+        const {correo, contraseña} = req.body;
+        
+        const usuario = await Usuario.findOne({correo}); // Busca en mongoDB un usuario cuyo correo coincida con el que enviaron 
+        if (!usuario) {
+            return res.status(404).json({mensaje: "Usuario no encontrado"}); // Corta la ejecución si no encontró a nadie, así el código de abajo no se ejecuta, con un usuario que no existe
+        }
+
+        const contraseñaValida = await bcrypt.compare(contraseña, usuario.contraseña);
+        if (!contraseñaValida) {
+            return res.status(401).json({mensaje: "Contraseña incorrecta"});
+        }
+
+        const token = jwt.sign(
+            {id: usuario._id,correo: usuario.correo},process.env.JWT_SECRET, {expiresIn: "2h"} // Para que el token deje de funcionar a las 2 horas
+        );
+
+        res.status(200).json({mensaje: "Inicio de sesión exitoso", token});
+
+    }   catch (error) {
+        res.status(500).json({mensaje: error.message});
+    }
+
+}
+
+module.exports = {registrar, login};
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {registrar} = require("../controllers/usuarioController");
+const {registrar, login} = require("../controllers/usuarioController");
 
 router.post("/registro", registrar);
+router.post("/login", login)
 
 module.exports = router;
